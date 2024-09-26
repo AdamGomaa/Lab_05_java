5c0ef18f8d27a960189375370798a2deb3e77139
AdamGomaa <GOMAAA@MAIL.UC.EDU>
2024-09-26 09:26:34 -0400

    Changes

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -5,7 +5,7 @@ public class BirthMonth {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your birth month (1-12): ");
         int month = scanner.nextInt();
-        if (month >= 1 && month <= 12) {
+        if (month >= 1 & month <= 12) {
             System.out.println("Your birth month is: " + month);
         }
         else {
--- src/ShippingCostCalculator.java
+++ src/ShippingCostCalculator.java
@@ -1,22 +1,23 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 import static java.lang.System.in;
 
 public class ShippingCostCalculator {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(in);
         double price = 100;
         System.out.print("Enter the item price: $");
         double itemPrice = scanner.nextDouble();
         double shippingFee = 0;
-        if (itemPrice > price) {
+        double totalCost;
+        if (itemPrice >= price) {
             System.out.print("Your shipping price is free ");
         } else {
+            totalCost = itemPrice + shippingFee;
             shippingFee = itemPrice * 0.02;
+            System.out.printf("Shipping Cost: $" + shippingFee);
         }
-        double totalCost = itemPrice + shippingFee;
-        System.out.printf("Shipping Cost: $" + shippingFee);
+        totalCost = itemPrice + shippingFee;
         System.out.printf(" Total Price: $" + totalCost);
     }
 }
